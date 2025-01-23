d726346fc4ae4ae454b37efdfa88d2123bb6d82b
JungHyeonKim <dev.kjung@gmail.com>
2025-01-23 23:30:05 +0900

    [Feat] create project

---
+++ src/main/java/com/example/logbookexample/LogbookExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.logbookexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbookExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbookExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/logbookexample/LogbookExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logbookexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbookExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
